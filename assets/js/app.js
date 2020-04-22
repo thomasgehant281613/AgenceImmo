a871e1d8bb027132a67585877da603babb6aba0d
thomasgehant281613 <thomasgehant@outlook.fr>
2020-04-22 19:29:15 +0200

    feat: Mise en place de symfony encore

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,17 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import $ from 'jquery';
+require('select2')
+
+$('select').select2()
+let $contactButton =$('#contactButton')
+$('#contactButton').click(e => {
+    e.preventDefault()
+    $('#contactForm').slideDown();
+    $contactButton.slideUp();
+})
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
